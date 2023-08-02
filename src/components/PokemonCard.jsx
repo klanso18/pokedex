ab0e04074548acdc18dfc0c8ef3c225b77637de3
Kevin Lansot <KEVIN.LANSOT@gmail.com>
2023-08-02 09:29:42 +0200

    pokemon image displays dynamically with condition

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,23 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+];
+
 function PokemonCard() {
+
+    const pokemon = pokemonList[0];
     return (
         <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-            alt="Elephant at sunset"/>
-        <figcaption>bulbasaur</figcaption>
+            {pokemon === pokemonList[0] ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
+            <figcaption>{pokemon.name}</figcaption>
         </figure>
-    );
+    )
 }
 
 export default PokemonCard;
