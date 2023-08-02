b8aa5375453527f87ff5876de5d34ab44d872f34
Kevin Lansot <KEVIN.LANSOT@gmail.com>
2023-08-02 15:21:33 +0200

    new prop pokemon display

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,24 @@ import './App.css'
 import MyTitle from "./components/MyTitle"
 import PokemonCard from './components/PokemonCard'
 
+const pokemonList = [
+  {
+    name: "Bulbizarre",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "Herbizarre",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
+  },
+];
+
 function App() {
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard name={pokemonList[0].name} imgSrc={pokemonList[0].imgSrc} />
+      <PokemonCard name={pokemonList[1].name} imgSrc={pokemonList[1].imgSrc} />
     </div>
   );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,21 +1,10 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-];
-
-function PokemonCard() {
-
-    const pokemon = pokemonList[0];
+function PokemonCard(props) {
+    
+    console.log(props);
     return (
         <figure>
-            {pokemon === pokemonList[0] ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
-            <figcaption>{pokemon.name}</figcaption>
+            <img src={props.imgSrc} alt={props.name} />
+            <figcaption>{props.name}</figcaption>
         </figure>
     )
 }
