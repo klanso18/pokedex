9e691de7153c2a7b74658c1217093d5ea8910e6c
Kevin Lansot <KEVIN.LANSOT@gmail.com>
2023-08-02 22:12:03 +0200

    propType added on PokemonCard component

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function App() {
   return (
     <div>
       <PokemonCard pokemon={pokemonList[0]} />
+      <PokemonCard pokemon={pokemonList[1]} />
     </div>
   );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 function PokemonCard({pokemon}) {
     
     console.log(pokemon);
@@ -7,6 +9,13 @@ function PokemonCard({pokemon}) {
             <figcaption>{pokemon.name}</figcaption>
         </figure>
     )
+}
+
+PokemonCard.propsTypes = {
+    pokemon: PropTypes.shape({
+        imgSrc: PropTypes.string,
+        name: PropTypes.string.isRequired,
+    }),
 }
 
 export default PokemonCard;
